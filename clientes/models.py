0c8094bd8a15bd7ec26d66a3cc79af4020a30b0a
emilianop235 <70235leo@universidaddeleon.edu.mx>
2026-06-17 17:51:48 -0600

    actualizacion de los modelos

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField()
+    apellido = models.CharField()
+    sexo = models.CharField()
+    tipo = models.CharField()
+    direccion = models.CharField()
