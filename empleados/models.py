26b9754db5233ba4973eacb8d91911f5edfd0c81
emilianop235 <70235leo@universidaddeleon.edu.mx>
2026-06-17 18:21:46 -0600

    modelos

--- empleados/models.py
+++ empleados/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Empleado(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    sexo = models.CharField(max_length=10)
+    tipo = models.CharField(max_length=50)
--- proveedores/models.py
+++ proveedores/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class proveedores(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    telefono = models.CharField(max_length=20)
+    
