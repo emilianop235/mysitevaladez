acd388295572f93f1ae0689d6f2d6578c137e680
emilianop235 <70235leo@universidaddeleon.edu.mx>
2026-07-01 16:55:10 -0600

    po

--- empleados/models.py
+++ empleados/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 
+import empleados
+
 class Empleado(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     sexo = models.CharField(max_length=10)
     tipo = models.CharField(max_length=50)
+
+
+class nomina(models.Model):
+    numperiodo = models.CharField()
+    fecha = models.DateField()
+    salario = models.FloatField()
+    percepciones = models.FloatField()
+    deducciones = models.FloatField()
+    total = models.FloatField()
+    empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE, related_name='nomina')
