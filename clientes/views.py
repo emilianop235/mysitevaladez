feb43f13ee359cc34c44257a80712d258b6fc3af
emilianop235 <70235leo@universidaddeleon.edu.mx>
2026-07-01 16:27:03 -0600

    borrado

--- clientes/views.py
+++ clientes/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def clientes(request):
-    #return HttpResponse("Formularios de clientes-emiliano")
     return render(request, 'clientes/clientes.html')
 
 
